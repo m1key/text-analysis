88f7b9798844b3c2e161595e2363c2bd592f9182
Mike Huniewicz <michal.huniewicz.registered@gmail.com>
2017-10-22 14:52:49 +0100

    Added pandas - stats for all galleries.

---
+++ mining.py
@@ -0,0 +1,47 @@
+from optparse import OptionParser
+parser = OptionParser()
+parser.add_option("-i", "--input", dest="input",
+                  help="CSV file with galleries",
+                  default="/tmp/galleries.csv")
+(options, args) = parser.parse_args()
+
+import pandas as pd
+df = pd.read_csv(options.input)
+
+print("Most words:")
+print(df.sort_values(['word_count'], ascending=[0]).head(10)[['name', 'word_count']])
+
+print("Fewest words:")
+print(df.sort_values(['word_count'], ascending=[1]).head(10)[['name', 'word_count']])
+
+print
+
+print("Most unique words:")
+print(df.sort_values(['unique_word_count'], ascending=[0]).head(10)[['name', 'unique_word_count']])
+
+print("Fewest unique words:")
+print(df.sort_values(['unique_word_count'], ascending=[1]).head(10)[['name', 'unique_word_count']])
+
+print
+
+print("Longest sentences:")
+print(df.sort_values(['word_per_sentence_count'], ascending=[0]).head(10)[['name', 'word_per_sentence_count']])
+
+print("Shortest sentences:")
+print(df.sort_values(['word_per_sentence_count'], ascending=[1]).head(10)[['name', 'word_per_sentence_count']])
+
+print
+
+print("Most positive galleries:")
+print(df.sort_values(['polarity'], ascending=[0]).head(10)[['name', 'polarity']])
+
+print("Least positive galleries:")
+print(df.sort_values(['polarity'], ascending=[1]).head(10)[['name', 'polarity']])
+
+print
+
+print("Most subjective galleries:")
+print(df.sort_values(['subjectivity'], ascending=[0]).head(10)[['name', 'subjectivity']])
+
+print("Least subjective galleries:")
+print(df.sort_values(['subjectivity'], ascending=[1]).head(10)[['name', 'subjectivity']])
