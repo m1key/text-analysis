a1a77f0fbcf50fbb1876fd6832f8f16ba1b525d2
Mike Huniewicz <michal.huniewicz.registered@gmail.com>
2017-10-22 13:15:39 +0100

    Analyse all galleries.

---
+++ analyse_all.py
@@ -0,0 +1,16 @@
+from os import listdir
+from os.path import isfile, join
+
+galleries_dir = '/run/media/mike/HUNIEWICZ/photography'
+gallery_dirs = [f for f in listdir(galleries_dir)
+    if not isfile(join(galleries_dir, f))]
+
+import analyser
+
+for gallery_dir in gallery_dirs:
+    gallery_index = join(galleries_dir, gallery_dir, 'gallery.yaml')
+    if isfile(gallery_index):
+        analyser.analyse_gallery_yaml(gallery_index)
+    else:
+        print('File %s does not exist.'% gallery_index)
+    print
